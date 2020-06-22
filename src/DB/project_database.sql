59493f7cbbbabac27a5de0e651dadf5a8b84fbf7
juanguren <juan_fe_7@live.com>
2020-06-21 22:17:12 -0500

    Created users folder

--- src/DB/project_database.sql
+++ src/DB/project_database.sql
@@ -61,7 +61,16 @@ CREATE TABLE order_items(
     FOREIGN KEY item_id REFERENCES items(item_id)
 )
 
+"The SQL EXISTS Operator
+The EXISTS operator is used to test for the existence of any record in a subquery.
 
+The EXISTS operator returns true if the subquery returns one or more records.
+
+EXISTS Syntax
+SELECT column_name(s)
+FROM table_name
+WHERE EXISTS
+(SELECT column_name FROM table_name WHERE condition);"
 
 
 
