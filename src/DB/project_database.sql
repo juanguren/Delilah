7f4bdb9460c41a25324ec1469f792726293e82a3
juanguren <juan_fe_7@live.com>
2020-06-21 19:18:24 -0500

    Updated database schema

--- src/DB/project_database.sql
+++ src/DB/project_database.sql
@@ -1,28 +1,71 @@
 
-/* user - table query*/
+/* users - table query*/
+CREATE TABLE users(
+    user_id int primary key AUTO_INCREMENT,
+    fullName varchar(20) not null,
+    email varchar(20) not null,
+    phone varchar(20) not null,
+    user_address varchar(20) not null,
+    user_password varchar(20) not null,
+    username varchar(20) not null,
+    is_admin varchar(5) not null
+)
 
+/* super_admin - table query */
+CREATE TABLE super_admin (
+	super_id int PRIMARY KEY,
+    fullName varchar(20) not null,
+    super_address varchar(25) not null,
+)
+
+/* admin - table query*/
+CREATE TABLE admin(
+
+)
 
 /* items - table query*/
 CREATE TABLE items (
-	items_id int PRIMARY KEY AUTO_INCREMENT,
+	item_id int PRIMARY KEY AUTO_INCREMENT,
     name varchar(40) NOT NULL,
     photo_url varchar(250) NOT NULL,
     price int NOT NULL,
     description varchar(80) NOT NULL,
     cooking_time time NOT NULL,
-    restaurants_id int,
-    FOREIGN KEY (restaurants_id) REFERENCES restaurants (restaurants_id)
+    quantity int NOT NULL
 )
 
-/* restaurants - table query */
-CREATE TABLE restaurants (
-	restaurants_id int PRIMARY KEY AUTO_INCREMENT,
-    name varchar(20) not null,
-    description varchar(100),
-    address varchar(25) not null,
-    category varchar(15) not null,
-    state varchar(8) not null
+/* canceled_order - table query*/
+CREATE TABLE canceled_order(
+    canceled_order_id int primary key AUTO_INCREMENT,
+    canceling_reason varchar(max),
+    canceling_time smalldatetime
 )
+
+/* orders - table query*/
+CREATE TABLE orders(
+    order_id int primary key AUTO_INCREMENT,
+    order_time smalldatetime not null,
+    arrival_time smalldatetime not null,
+    canceled_order_id int,
+    user_id int,
+    FOREIGN KEY user_id REFERENCES users(user_id),
+    FOREIGN KEY canceled_order_id REFERENCES canceled_order(canceled_order_id)
+)
+
+/* order_items - table query*/
+CREATE TABLE order_items(
+    order_item_id int primary key AUTO_INCREMENT,
+    order_id int,
+    item_id int,
+    FOREIGN KEY user_id REFERENCES users(user_id),
+    FOREIGN KEY item_id REFERENCES items(item_id)
+)
+
+
+
+
+
+/* =============  OPTIONAL TABLES =============  */
 
 /* pickers - table query */
 CREATE TABLE pickers(
