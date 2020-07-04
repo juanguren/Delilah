92f473930416e568e961409ae75f48253f494ca3
juanguren <juan_fe_7@live.com>
2020-07-04 16:29:00 -0500

    Added an item creation endpoint + a middleware that validates the uploaded item is not repeated.

--- src/routes/items/item_registry.js
+++ src/routes/items/item_registry.js
@@ -6,19 +6,45 @@ const { sequelize, Sequelize } = require("../../../server");
 
 router.use(body_parser.json());
 
-router.post("/create_item", (req, res) =>{
+function checkUniqueItem (req, res, next){
+    const {item_code} = req.body;
+    console.log(item_code);
+    sequelize.query('SELECT * FROM items WHERE item_code = :code ',{
+        type: Sequelize.QueryTypes.SELECT,
+        replacements: {
+            code: item_code
+        }
+    }).then((response) =>{
+        if (response == "") {
+            next();
+        } else{
+            res.status(404).json({err: "Ups. The uploaded item is repeated"});
+        }
+    });
+}
+
+router.post("/create_item", checkUniqueItem, (req, res) =>{
     
-    const {name, photo_url, price, description, cooking_time} = req.body;
+    const {name, photo_url, price, item_description, cooking_time, quantity, item_code} = req.body;
     
-    sequelize.query('INSERT INTO items VALUES (NULL, :name, :photo_url, :price, :description, :cooking_time)', {
+    sequelize.query('INSERT INTO items VALUES (NULL, :name, :photo_url, :price, :item_description, :cooking_time, :quantity, :item_code)', {
         replacements: {
             name: name,
             photo_url: photo_url,
             price: price,
-            description: description,
-            cooking_time: cooking_time
+            item_description: item_description,
+            cooking_time: cooking_time,
+            quantity: quantity,
+            item_code: item_code
         }
-    }).then(response => console.log(response)) 
+    }).then((response) =>{
+        if (response = "") {
+            res.status(400).json({err: "There was an error loading this query. \n please try again or" + 
+        " contact your admin"});
+        } else{
+            res.status(200).json({msg: `Item ${name} uploaded succesfuly`})
+        }
+    }).catch(err => res.json({err: err}));
 });
 
 module.exports = router;
