2565859a3a6f14ec02bf7de366af50bea293c5d4
juanguren <juan_fe_7@live.com>
2020-07-04 18:11:24 -0500

    Added an index to each route with an endpoint that logs a welcoming message

--- server.js
+++ server.js
@@ -7,15 +7,20 @@ module.exports = {sequelize, Sequelize};
 const app = express();
 
 // Routes
-app.use("/", require("./src/routes/users/user_check"));
-app.use("/", require("./src/routes/users/user_signup"));
-app.use("/", require("./src/routes/items/item_registry"));
-app.use("/", require("./src/routes/items/item-check"));
-app.use("/", require("./src/routes/orders/create-order"));
-app.use("/", require("./src/routes/orders/cancel-order"));
-app.use("/", require("./src/routes/orders/view-orders"));
-app.use("/", require("./src/routes/admins/create-super"));
-app.use("/", require("./src/routes/admins/create-admins"));
+app.use("/delilah", require("./src/routes/users/user_check"));
+app.use("/delilah", require("./src/routes/users/user_signup"));
+app.use("/delilah", require("./src/routes/items/item_registry"));
+app.use("/delilah", require("./src/routes/items/item-check"));
+app.use("/delilah", require("./src/routes/orders/create-order"));
+app.use("/delilah", require("./src/routes/orders/cancel-order"));
+app.use("/delilah", require("./src/routes/orders/view-orders"));
+app.use("/delilah", require("./src/routes/admins/create-super"));
+app.use("/delilah", require("./src/routes/admins/create-admins"));
+
+app.get("/delilah", (req, res) =>{
+    res.json({msg: "Welcome to Delilah!",
+    us: "We are a new and revolutionary service that gives you time. Therefore, freedom."});
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
