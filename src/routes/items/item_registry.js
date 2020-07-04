91416d4d70cdecc1660cf9c5f4bb46e1fc520a59
juanguren <juan_fe_7@live.com>
2020-07-04 16:39:43 -0500

    Validate all fields in the body are complete

--- src/routes/items/item_registry.js
+++ src/routes/items/item_registry.js
@@ -26,25 +26,29 @@ function checkUniqueItem (req, res, next){
 router.post("/create_item", checkUniqueItem, (req, res) =>{
     
     const {name, photo_url, price, item_description, cooking_time, quantity, item_code} = req.body;
-    
-    sequelize.query('INSERT INTO items VALUES (NULL, :name, :photo_url, :price, :item_description, :cooking_time, :quantity, :item_code)', {
-        replacements: {
-            name: name,
-            photo_url: photo_url,
-            price: price,
-            item_description: item_description,
-            cooking_time: cooking_time,
-            quantity: quantity,
-            item_code: item_code
-        }
-    }).then((response) =>{
-        if (response = "") {
-            res.status(400).json({err: "There was an error loading this query. \n please try again or" + 
-        " contact your admin"});
-        } else{
-            res.status(200).json({msg: `Item ${name} uploaded succesfuly`})
-        }
-    }).catch(err => res.json({err: err}));
+
+    if (req.body) {
+        sequelize.query('INSERT INTO items VALUES (NULL, :name, :photo_url, :price, :item_description, :cooking_time, :quantity, :item_code)', {
+            replacements: {
+                name: name,
+                photo_url: photo_url,
+                price: price,
+                item_description: item_description,
+                cooking_time: cooking_time,
+                quantity: quantity,
+                item_code: item_code
+            }
+        }).then((response) =>{
+            if (response = "") {
+                res.status(400).json({err: "There was an error loading this query. \n please try again or" + 
+            " contact your admin"});
+            } else{
+                res.status(200).json({msg: `Item ${name} uploaded succesfuly`})
+            }
+        }).catch(err => res.json({err: err}));
+    } else{
+        res.status(400).json({err: "Check all the fields are complete."})
+    }
 });
 
 module.exports = router;
