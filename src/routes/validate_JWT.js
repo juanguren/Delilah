974c415040352c87e29031419b39e55d536f3507
juanguren <juan_fe_7@live.com>
2020-07-19 16:06:10 -0500

    Exported the JWT function for modularization purposes

---
+++ src/routes/validate_JWT.js
@@ -0,0 +1,26 @@
+
+const { sequelize, Sequelize } = require("../../server");
+const jwt = require("jsonwebtoken");
+const signature = "mySignature";
+
+let validateWithJWT = (req, res, next) =>{
+    const { username, password } = req.body;
+    if (req.body) {
+        sequelize.query('SELECT * FROM `super_admin` WHERE username = :username AND password = :password',{
+            type: Sequelize.QueryTypes.SELECT,
+            replacements:{
+                username,
+                password
+            }
+        }).then((response) =>{
+            if (response) {
+                req.params.token = jwt.sign(username, signature);
+                next();
+            } else{
+                res.json({err: "Invalid credentials"});
+            }
+        }).catch(err => console.log(err))
+    }
+}
+
+module.exports = validateWithJWT;
