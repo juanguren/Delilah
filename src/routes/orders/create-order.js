10c90af90d423613e0d7cdeefcb4e96ce528702a
juanguren <juan_fe_7@live.com>
2020-08-17 13:42:02 -0500

    Imported Sequelize object

--- src/routes/orders/create-order.js
+++ src/routes/orders/create-order.js
@@ -6,4 +6,20 @@ const { sequelize, Sequelize } = require("../../../server");
 
 router.use(body_parser.json());
 
+const isUserLoggedIn = (req, res, next) =>{
+    const {username} = req.body;
+    sequelize.query('SELECT * from users WHERE username = :username AND isLogged = "true"',{
+        type: Sequelize.QueryTypes.SELECT,
+        replacements: {
+            username
+        }
+    }).then((response) =>{
+        console.log(response);
+    })
+}
+
+router.post("/order", isUserLoggedIn, (req, res) =>{
+    
+})
+
 module.exports = router;
