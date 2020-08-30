5e7cf5d2b0472361b5aa92d5b7576aa7c2fd272e
juanguren <juan_fe_7@live.com>
2020-08-29 21:31:16 -0500

    Order endpoint (in progress)

--- src/routes/orders/create-order.js
+++ src/routes/orders/create-order.js
@@ -3,22 +3,44 @@ const express = require("express");
 const body_parser = require("body-parser");
 const router = express.Router();
 const { sequelize, Sequelize } = require("../../../server");
+const authUser = require("../validations/authUser");
 
 router.use(body_parser.json());
 
 const isUserLoggedIn = (req, res, next) =>{
-    const {username} = req.body;
-    sequelize.query('SELECT * from users WHERE username = :username AND isLogged = "true"',{
+    const username = req.params.loggedUser;
+    sequelize.query('SELECT username from users WHERE username = :username AND isLogged = "true"',{
         type: Sequelize.QueryTypes.SELECT,
         replacements: {
             username
         }
     }).then((response) =>{
-        console.log(response);
+        if (response[0].username === username) {
+            next();
+        } else{
+            res.status(400).json({err: "Incorrect user credentials"});
+        }
+    }).catch(err => res.status(400).json(err))
+}
+
+const makeOrder = (req, res, next) =>{
+    const user = req.params.loggedUser;
+    sequelize.query('SELECT user_id FROM users WHERE username = :user',{
+        type: Sequelize.QueryTypes.SELECT,
+        replacements:{
+            user
+        }
+    }).then((res) =>{
+        const {user_id} = res[0]; // ! Create a utils folder for this dummy data
+        sequelize.query('INSERT INTO orders(order_time, arrival_time, order_status, canceled_order_id. user_id) VALUES(?, ?, ?, ?, ?)',{
+            replacements: [
+
+            ]
+        })
     })
 }
 
-router.post("/order", isUserLoggedIn, (req, res) =>{
+router.post("/order", [authUser, isUserLoggedIn], (req, res) =>{
     
 })
 
