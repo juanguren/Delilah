095f9c64bf3a0300d055ff604021fab5db28b781
juanguren <juan_fe_7@live.com>
2020-09-06 19:09:58 -0500

    Validations for the admin order search module

--- src/routes/orders/view-orders.js
+++ src/routes/orders/view-orders.js
@@ -3,10 +3,35 @@ const express = require("express");
 const body_parser = require("body-parser");
 const router = express.Router();
 const { sequelize, Sequelize } = require("../../../server");
+const authUser = require("../../validations/authUser");
 
 router.use(body_parser.json());
 
-function orderbyID(req, res, next) {
+const isAdminLoggedIn = (req, res, next) =>{
+    const username = req.params.loggedUser;
+    sequelize.query('SELECT username from admins WHERE username = :username AND isLogged = "true"',{
+        type: Sequelize.QueryTypes.SELECT,
+        replacements: {
+            username
+        }
+    }).then((response) =>{
+        if (response[0].username === username) {
+            next();
+        } else{
+            res.status(400).json({err: "Incorrect user credentials"});
+        }
+    }).catch(err => res.status(400).json(err))
+}
+
+router.get("/orders", [authUser, isAdminLoggedIn], (req, res) =>{
+    sequelize.query('SELECT * FROM orders',{
+        type: Sequelize.QueryTypes.SELECT
+    }).then((orders) =>{
+        res.status(200).json({orders});
+    }).catch(err => res.status(400).json(err));
+});
+
+function orderbyID (req, res, next) {
     let ID = req.params.id;
 
     sequelize.query('SELECT * FROM orders where order_id = :id',{
@@ -24,15 +49,7 @@ function orderbyID(req, res, next) {
     }).catch(err => res.status(400).json(err));
 }
 
-router.get("/orders", (req, res) =>{
-    sequelize.query('SELECT * FROM orders',{
-        type: Sequelize.QueryTypes.SELECT
-    }).then((orders) =>{
-        res.status(200).json({orders});
-    }).catch(err => res.status(400).json(err));
-});
-
-router.get("/orders/:id", orderbyID, (req, res) =>{
+router.get("/orders/:id", [isAdminLoggedIn, orderbyID], (req, res) =>{
     let foundOrder = req.params.found;
     console.log(foundOrder);
     res.status(200).json(foundOrder);
