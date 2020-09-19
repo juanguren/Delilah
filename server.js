5b151efcf7c907666a196f7f07480f173ed2d182
juanguren <juan_fe_7@live.com>
2020-09-19 16:27:11 -0500

    Code simplification and the beginning of route upgrades

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const express = require("express");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize('mysql://root@localhost:3306/delilah');
 module.exports = {sequelize, Sequelize};
+const router = express.Router();
+
+const routes = "./src/routes/";
 
 const app = express();
 
 // Routes
-app.use("/delilah", require("./src/routes/users/user_check"));
-app.use("/delilah", require("./src/routes/users/user_signup"));
-app.use("/delilah", require("./src/routes/users/user-login"));
-app.use("/delilah", require("./src/routes/items/item_registry"));
-app.use("/delilah", require("./src/routes/items/item-check"));
-app.use("/delilah", require("./src/routes/orders/create-order"));
-app.use("/delilah", require("./src/routes/orders/cancel-order"));
-app.use("/delilah", require("./src/routes/orders/view-orders"));
-app.use("/delilah", require("./src/routes/admins/create-super"));
-app.use("/delilah", require("./src/routes/admins/create-admins"));
-app.use("/delilah", require("./src/routes/admins/adminActions"));
+app.use("/delilah", require(routes + "users/user_check"));
+app.use("/delilah", require(routes + "users/user_signup"));
+app.use("/delilah", require(routes + "users/user-login"));
+app.use("/delilah", require(routes + "items/item_registry"));
+app.use("/delilah", require(routes + "items/item-check"));
+app.use("/delilah", require(routes + "orders/create-order"));
+app.use("/delilah", require(routes + "orders/cancel-order"));
+app.use("/delilah", require(routes + "orders/view-orders"));
+app.use("/delilah", require(routes + "admins/create-super"));
+app.use("/delilah", require(routes + "admins/create-admins"));
+//app.use("/delilah", require(routes + "admins/admin-actions"));
 
 app.get("/delilah", (req, res) =>{
     res.json({msg: "Welcome to Delilah!",
