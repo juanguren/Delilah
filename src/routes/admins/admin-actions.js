2dca45d796c6b66ba9b712ce8ae7616c91de5d64
juanguren <juan_fe_7@live.com>
2020-09-20 14:21:38 -0500

    Independant cancellation endpoint with its own middleware

--- src/routes/admins/admin-actions.js
+++ src/routes/admins/admin-actions.js
@@ -13,41 +13,48 @@ const updateOrderStatus = (req, res, next) =>{
 
     const acceptedStatus = [
         'PENDING',
-        'IN PROGRESS',
-        'CANCELLED'
+        'IN PROGRESS'
     ]
-    acceptedStatus.includes(order_status) ? next() : res.status(400).json({msg: 'Incorrect status entry. Please input a valid one'})
+    acceptedStatus.includes(order_status) ? next() : res.status(400).json({msg: 'Incorrect status entry. Please input a valid one', acceptedStatus});
 }
 
-const isOrderCancelled = (req, res, next) =>{
+router.put("/order/:id/:status", updateOrderStatus, (req, res) =>{
+    const order_id = req.params.id;
+    const newStatus = req.params.status;
+
+    sequelize.query('UPDATE orders SET order_status = :newStatus WHERE order_uuid = :order_id', {
+        replacements: { newStatus, order_id }
+    }).then(() =>{
+        res.status(200).json({msg: 'Order succesfully updated'})
+    }).catch((e) => res.status(400).json(e));
+});
+
+
+const cancelOrder = (req, res, next) =>{
     const id = req.params.id;
-    const order_status = req.params.status;
     const { reason } = req.body;
 
     if(reason){
-        if (order_status = 'CANCELLED') {
-            sequelize.query('SELECT order_id FROM orders WHERE order_uuid = :order_id', {
-                type: Sequelize.QueryTypes.SELECT,
-                replacements: { order_id : id }
-            }).then((response) =>{
-                const { order_id } = response[0];
-                sequelize.query('INSERT INTO canceled_order VALUES(:order_id, :reason, :time)',{
-                    replacements: {
-                        order_id,
-                        reason,
-                        time: orderTime()
-                    }
-                }).then(() => { next(); })
-            }).catch((error) => res.status(400).json({msg: 'Couldn´t update order as cancelled', error}))
-        } else{
-            next();
-        }
-    } else { res.status(404).json({msg: 'No reson provided'}) }
+        sequelize.query('SELECT order_id FROM orders WHERE order_uuid = :order_id', {
+            type: Sequelize.QueryTypes.SELECT,
+            replacements: { order_id : id }
+        }).then((response) =>{
+            const { order_id } = response[0];
+            sequelize.query('INSERT INTO canceled_order VALUES(:order_id, :reason, :time)',{
+                replacements: {
+                    order_id,
+                    reason,
+                    time: orderTime()
+                }
+            }).then(() => { next(); })
+        }).catch((error) => res.status(400).json({msg: 'Couldn´t update order as cancelled', error}))
+
+    } else { res.status(404).json({msg: 'No reason provided'}) }
 }
 
-router.put("/order/:id/:status", [updateOrderStatus, isOrderCancelled], (req, res) =>{
+router.put("order/:id/cancel", cancelOrder, (req, res) =>{
     const order_id = req.params.id;
-    const newStatus = req.params.status;
+    const newStatus = 'CANCELLED';
 
     sequelize.query('UPDATE orders SET order_status = :newStatus WHERE order_uuid = :order_id', {
         replacements: { newStatus, order_id }
